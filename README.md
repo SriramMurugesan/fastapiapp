7619a4390b8a4270f3812e87d097ab957ca2f712
srirammurugesan <sri2014ram@gmail.com>
2026-06-29 12:08:57 +0530

    alembic

--- README.md
+++ README.md
@@ -88,5 +88,12 @@ CREATE TABLE Students(
 
 pip install alembic
 alembic init alembic
+alembic-> env.py -> from imported model ->metadata data
+alembic.ini->sqlalchemy.url to postgresql database url ---> postgresql://user:password@host:port/database_name
 alembic revision --autogenerate -m "initial migration"
+alembic upgrade head   
+
+
+
+
 
--- alembic/env.py
+++ alembic/env.py
@@ -1,8 +1,13 @@
 from logging.config import fileConfig
+import sys
+import os
+
+# Add the project root directory to sys.path so that
+# 'database', 'models', etc. can be imported correctly.
+sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -16,9 +21,10 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from database import Base
+from models.company import Company
+from models.job import Job    
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
